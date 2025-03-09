1950307402a98c5bd86c478d7c6f5b71b7e1f3a8
AgneBvdB <Agne.banzeryte@gmail.com>
2025-03-09 18:19:35 +0100

    Create countdown.js
    Signed-off-by: AgneBvdB <Agne.banzeryte@gmail.com>

---
+++ countdown.js
@@ -0,0 +1,19 @@
+const countDate = new Date("Aug 9, 2025 00:00:01").getTime();
+let counter = document.getElementById("counter");
+
+const intervalCounter = setInterval(function() {
+  let currentDate = new Date().getTime();
+  let distance = countDate - currentDate;
+
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 *60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+  counter.innerText = `${days} DAYS ${hours} HOURS ${minutes} MINUTES ${seconds} SECONDS`;
+
+  if(distance < 0){
+    clearInterval(intervalCounter);
+    counter.innerText = "Finished!";
+  }
+}, 1000);
